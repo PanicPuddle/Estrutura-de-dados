f1098ff7d73766076f14e3824bee95d70d274557
PanicPuddle <minecraft3127@gmail.com>
2023-08-12 11:02:28 -0300

    Atividade1

---
+++ atividade1/verifica_numero_primo.c
@@ -0,0 +1,13 @@
+#include <stdbio.h>
+
+
+int verifica_numero_primo(int n){
+    return 0;
+}
+int main(int argc, char* argv[]){
+    int n;
+    printf("teste");
+
+    printf("\n%s",argv[1]);
+}
+
